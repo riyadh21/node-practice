9c8b947c6a5022ab6891a3e7aa290abd79697bd9
Riyadh ul islam <riyadh21@gmail.com>
2014-10-04 23:52:27 +0600

    add

---
+++ server.js
@@ -0,0 +1,22 @@
+var http = require("http");
+var url = require("url");
+var port = 8888;
+
+function start(route){
+	function onRequest(request , response){
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " Recieved.");
+
+		route(pathname);
+
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		//console.log(response);
+		response.end();
+	};
+
+	http.createServer(onRequest).listen(port);
+	console.log("Server has started on : " + port);
+}
+
+exports.start = start;
