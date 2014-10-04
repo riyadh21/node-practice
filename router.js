21814622a45a4b0277d8f33654a76201c221e2cc
Riyadh ul islam <riyadh21@gmail.com>
2014-10-05 00:58:26 +0600

    change in router

--- router.js
+++ router.js
@@ -1,11 +1,11 @@
-function route(pathname, handle){
-	console.log("about to route a request for : " + pathname);
-
+function route(handle, pathname) {
+	console.log("About to route a request for " + pathname);
 	if (typeof handle[pathname] === 'function') {
-		handle[pathname]();
+		return handle[pathname]();
 	} else {
 		console.log("No request handler found for " + pathname);
-	}	
+		return "404 Not found";
+	}
 }
 
 exports.route = route;
