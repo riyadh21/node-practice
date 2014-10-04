e28ec6beef35ba987bce62de1acff3025dc51285
Riyadh ul islam <riyadh21@gmail.com>
2014-10-05 00:43:31 +0600

    add handeler and some changes in route so that we can understand how handeler works

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var http = require("http");
 var url = require("url");
 var port = 8888;
 
-function start(route){
+function start(route, handle){
 	function onRequest(request , response){
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " Recieved.");
 
-		route(pathname);
+		route(pathname, handle);
 
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
