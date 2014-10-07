6cb690383e63f2d102c165c8953923db8e0e1412
Riyadh ul islam <riyadh21@gmail.com>
2014-10-07 12:44:19 +0600

    add a exec shall script to show how to work without enterapt, howerer it not return the exatc result we expect

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,23 @@
+var exec = require("child_process").exec;
+
 function start() {
 	console.log("Request handler 'start' was called.");
+	var content = "Empty";
 
-	return "Hello Start";
+	exec("ls -lah", function (error, stdout, stderr) {
+		content = stdout;
+		console.log(error);
+		console.log(stdout);
+	});
+/*
+	function sleep(milliSeconds) {
+		var startTime = new Date().getTime();
+		while (new Date().getTime() < startTime + milliSeconds);
+	}
+	
+	sleep(10000);
+*/	
+	return content;
 }
 
 function upload() {
