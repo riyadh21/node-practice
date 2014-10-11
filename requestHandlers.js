ca07afdefd80ddf1c21f41649a816410940292d7
Riyadh ul islam <riyadh21@gmail.com>
2014-10-11 20:10:08 +0600

    add html it start method and show the simple text area and submit and form submit to upload api call

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,7 +2,22 @@ var exec = require("child_process").exec;
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
-
+	var body = '<html>'+
+				'<head>'+
+				'<meta http-equiv="Content-Type" content="text/html; '+
+				'charset=UTF-8" />'+
+				'</head>'+
+				'<body>'+
+				'<form action="/upload" method="post">'+
+				'<textarea name="text" rows="20" cols="60"></textarea>'+
+				'<input type="submit" value="Submit text" />'+
+				'</form>'+
+				'</body>'+
+				'</html>';
+	response.writeHead(200, {"Content-Type": "text/html"});			
+	response.write(body);
+	response.end();	
+/*				
 	exec("find /",
 		{timeout : 10000, maxBuffer : 20000*1024},
 		function (error, stdout, stderr) {
@@ -10,6 +25,7 @@ function start(response) {
 		response.write(stdout);
 		response.end();
 	});
+*/
 /*
 	function sleep(milliSeconds) {
 		var startTime = new Date().getTime();
