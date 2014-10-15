f46e3b3ad1d8cdd56d32549a68c170396b3d21a0
Riyadh ul islam <riyadh21@gmail.com>
2014-10-15 17:54:38 +0600

    add temp folder and add a image to that folder to show the saved image with show function

--- fileTest.js
+++ fileTest.js
@@ -6,10 +6,10 @@ http.createServer(function(req, res) {
 	if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
 		// parse a file upload
 		var form = new formidable.IncomingForm();
-		form.parse(req, function(error, fields, files) {
-		res.writeHead(200, {'content-type': 'text/plain'});
-		res.write('received upload:\n\n');
-		res.end(sys.inspect({fields: fields, files: files}));
+			form.parse(req, function(error, fields, files) {
+			res.writeHead(200, {'content-type': 'text/plain'});
+			res.write('received upload:\n\n');
+			res.end(sys.inspect({fields: fields, files: files}));
 		});
 		return;
 	}
